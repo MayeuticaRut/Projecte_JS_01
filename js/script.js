29b1bc37c1b937afc64fbfea78ad1ca507a1f065
MayeuticaRut <rutsdl@gmail.com>
2024-05-11 18:23:53 +0200

    Iniciado Projecte01

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/* 
+let menu = document.querySelector(".navegacion")
+(menu.style.display === "none") {
+    menu.classList.remove()"ocultar";
+    menu.classList.add("mostrar");
+} else {
+
+
+}*/
+
+
+// en css
+// .navegacion {
+    // display:??;
+// }
