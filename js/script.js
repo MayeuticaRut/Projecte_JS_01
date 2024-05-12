e23b14fe95fd56d78de7e5447d83a63e9292aedd
MayeuticaRut <rutsdl@gmail.com>
2024-05-13 01:20:00 +0200

    nuevos html

--- js/script.js
+++ js/script.js
@@ -1,31 +1,41 @@
+// DECLARANDO E INICIALIZANDO VARIABLES
+const boton = document.getElementById("boton");
+const navMenu = document.getElementById("nav_menu");
+const botonMenuEmpresa = document.getElementById("empresa_page");
+const botonMenuEscuela = document.getElementById("escuela_page");
 
-const boton= document.getElementById("boton");
-const navMenu= document.getElementById("nav_menu");
-const botonMenuEmpresa= document.getElementById("empresa_page"); 
-
-
+// CONTROLANDO MIS AVANCES POR CONSOLA
+console.log(botonMenuEscuela);
 console.log(botonMenuEmpresa);
 console.log(boton);
 console.log(navMenu);
-var style= window.getComputedStyle(navMenu);
-var displayStyle= style.display;
 
-console.log(botonMenuEmpresa);
+var style = window.getComputedStyle("navMenu");
+var displayStyle = style.display;
 
-function mostrarONo(params) {
-    if (navMenu.style.display == "none") {
-        navMenu.style.display = 'block';
-    }else {
-navMenu.style.display = "none";
-    }
-    
+
+// FUNCION PARA EL MENU HAMBURGUESA
+function mostrarONo() {
+  if (navMenu.style.display == "none") 
+    navMenu.style.display = "block";
+  else 
+    navMenu.style.display = "none";
+  
 }
+// FUNCIONES PARA MOSTRAR ENLACES
 function irEmpresaPage() {
-    window.location.href="project01_empresa.html";
+  window.location.href = "project01_empresa.html";
 }
+function irEscuelaPage() {
+    window.location.href = "project01_escuela.html";
+}
+
+// CONTROLANDO MIS AVANCES POR CONSOLA
 console.log(irEmpresaPage);
-botonMenuEmpresa,addEventListener("click", irEmpresaPage);
+console.log(irEscuelaPage);
+console.log(mostrarONo);
+
+// LLAMADA DE EVENTOS CLICK
+botonMenuEmpresa.addEventListener("click", irEmpresaPage);
+botonMenuEscuela.addEventListener("click", irEscuelaPage);
 boton.addEventListener("click", mostrarONo);
-
-
-
