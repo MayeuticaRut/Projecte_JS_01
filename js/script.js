56c94edba40d2884ad1590c857f625a78e7d3f8e
MayeuticaRut <rutsdl@gmail.com>
2024-05-12 12:37:25 +0200

    modificacion de index movil style

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 
-var boton= document.getElementById("boton");
-var navMenu= document.getElementById("nav_menu");
+const boton= document.getElementById("boton");
+const navMenu= document.getElementById("nav_menu");
+const botonMenuEmpresa= document.getElementById("empresa_page"); 
+
+
+console.log(botonMenuEmpresa);
 console.log(boton);
 console.log(navMenu);
 var style= window.getComputedStyle(navMenu);
 var displayStyle= style.display;
 
-console.log(displayStyle);
+console.log(botonMenuEmpresa);
 
 function mostrarONo(params) {
     if (navMenu.style.display == "none") {
@@ -16,8 +20,11 @@ navMenu.style.display = "none";
     }
     
 }
-
-
+function irEmpresaPage() {
+    window.location.href="project01_empresa.html";
+}
+console.log(irEmpresaPage);
+botonMenuEmpresa,addEventListener("click", irEmpresaPage);
 boton.addEventListener("click", mostrarONo);
 
 
