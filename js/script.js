8047948ae940127198f0869d7c4bc20119ab67ba
MayeuticaRut <rutsdl@gmail.com>
2024-05-19 15:47:19 +0200

    optimizo el css general, funciona el carrousel

--- js/script.js
+++ js/script.js
@@ -1,116 +1,77 @@
-
 // DECLARANDO E INICIALIZANDO VARIABLES
 const boton = document.getElementById("boton");
+let $botonAvanzar, $botonRetroceder;
 const navMenu = document.getElementById("nav_menu");
+const div = document.getElementById("divParaImg");
+let img = document.createElement("img");
+let posicionActual = 0;
+let descripcionActual = 0;
+let fotos = [
+  "../img/foto1-pase.jpg",
+  "../img/foto2-pase.jpg",
+  "../img/foto3-pase.jpg",
+];
 
 
-// CONTROLANDO MIS AVANCES POR CONSOLA
-
-// console.log(boton);
-// console.log(navMenu);
-
-// var style = window.getComputedStyle("navMenu");
-// var displayStyle = style.display;
-
-
-// FUNCION PARA EL MENU HAMBURGUESA
+///////////// FUNCION PARA EL MENU HAMBURGUESA ///////////////////
 function mostrarONo() {
-  if (navMenu.style.display == "none"){ 
+  if (navMenu.style.display == "none") {
     navMenu.style.display = "flex";
-}else{ 
-  navMenu.style.display = "none";
+  } else {
+    navMenu.style.display = "none";
+  }
 }
-};
-
-
-
-let botonAtributo = boton.setAttribute("target", "_blank");
-// console.log(botonAtributo);
-
-// CONTROLANDO MIS AVANCES POR CONSOLA
-
-console.log(mostrarONo);
 
 // LLAMADA DE EVENTOS CLICK
-
 boton.addEventListener("click", mostrarONo);
 
+///////////////// CARRUSEL /////////////
 
+// 
+$botonAvanzar =document.getElementById('avanzar');
+$botonRetroceder =document.getElementById('retroceder');
 
-
-// CARRUSEL
-
-let rutaSrc= document.querySelector(".foto").getAttribute("src");
-let newAtr= document.querySelector(".foto");
-newAtr.setAttribute("alt", "descripcion");
-
-// ejecuta todo esto cuando el navegador esté cargado
-// es anonima y se autoejecuta
-window.onload = function () {
-  // push y pop
-
-  //  let foto1 =
-  //  let foto2 =
-  //  let foto3 =
-// almacenamos la imagen
-  let fotos = ["foto1-pase.jpg", "foto2-pase.jpg", "foto3-pase1@3x.jpg"];
-
-  // 
-// console.log(fotos);
-// console.log(fotos[0]);
-// console.log(fotos[1]);
-// console.log(fotos[2]);
-
-
-  let descripcion = ["descripcion1", "descripcion2", "descripcion3"];
-
-  let posicionActual = 0;
-  let descripcionActual = 0;
-
-  let $botonRetroceder = document.getElementById("retroceder");
-  let $botonAvanzar = document.getElementById("avanzar");
-
-  // FUNCIONES AVANZAR Y RETROCEDER
-
-  function avanzarFoto() {
-    if (
-      posicionActual >= fotos.length - 1 &&
-      descripcionActual >= descripcion.length - 1
-    ) {
-      posicionActual = 0;
-      descripcionActual = 0;
-    } else {
-      posicionActual++;
-      descripcionActual++;
-    }
-    renderizarImagen();
-  };
-
-  function retrocederFoto() {
-    if (posicionActual <= 0 && descripcionActual <= 0) {
-      posicionActual = fotos.length - 1;
-      descripcionActual = descripcion.length - 1;
-    } else {
-      posicionActual--;
-      descripcionActual--;
-    }
-
-    renderizarImagen();
-  };
-
-  // pinta la imagen en el navegador
-  function renderizarImagen() {
-    document.getElementById("imagen").innerHTML =
-    `<img src="img/${fotos[posicionActual]}" alt="${descripcion[descripcionActual]}">`;
+// FUNCIONES AVANZAR Y RETROCEDER
+function avanzarFoto() {
+  if ( posicionActual >= (fotos.length - 1) ) {
+    posicionActual = 0;
+    // descripcionActual = 0;
+  } else {
+    posicionActual= posicionActual + 1;
+    // descripcionActual++;
   }
-  
-  //+++++++++ EVENTOS +++++++++++
+  renderizarImagen();
+}
 
-  $botonAvanzar.addEventListener("click", avanzarFoto);
-  $botonRetroceder.addEventListener("click", retrocederFoto);
+function retrocederFoto() {
+  if (posicionActual <= 0) {
+    posicionActual = fotos.length - 1;
+    // descripcionActual = descripcion.length - 1;
+  } else {
+    posicionActual--;
+    // descripcionActual--;
+  }
+
+  renderizarImagen();
+}
+
+function renderizarImagen() {
+  img.src = fotos[posicionActual];
+  div.innerHTML = ''; // Limpiar el div antes de agregar la nueva imagen
+  div.appendChild(img);
+  console.log('Imagen mostrada:', img.src);
+}
 
 // ++++++++ INICIALIZAR +++++++++
 renderizarImagen();
 
-};
-// FIN FUNCION WINDOW ONLOAD
+//+++++++++ EVENTOS +++++++++++
+$botonAvanzar.addEventListener("click", function(event) {
+  event.preventDefault(); // Evitar que el enlace navegue
+  avanzarFoto();
+});
+
+$botonRetroceder.addEventListener("click", function(event) {
+  event.preventDefault(); // Evitar que el enlace navegue
+  retrocederFoto();
+});
